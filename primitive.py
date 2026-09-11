df2897be110a0db2c377ff9094fa95dfc91eb39a
Xusrav0 <xusravshohidjonov@gmail.com>
2026-09-11 17:01:47 +0900

    feat: learning python booleans

--- primitive.py
+++ primitive.py
@@ -27,4 +27,30 @@ print(f"the result (3): {result}")
 
 result = course.replace("Fullstack", "MasterClass")
 print(f"the result (4): {result}")
-print(course)
+
+
+# course = course.replace("Fullstack", "MasterClass")
+# print(f"the result (4): {result}")
+# uziga tenglaganda shunda course qiymati uzgaradi
+
+print('==== boolean ====')
+# functions => type() input() bool() int() str()
+
+y = input("Give your value for y:")
+print(y)
+
+result = y.isnumeric()
+print(f"the input value is numeric: {result}")
+
+# Truthy vs Falsy value
+
+# TRUTHY > True 100 -100 "MIT"
+# FALSY > False 0 "" None
+
+
+test_falsy = "" or False or None or 0
+# agar or 100 qo'yilsa natija True bo'ladi
+print("The FALSY:", bool(test_falsy))
+
+test_truthy = "MIT"
+print("test_truthy:", bool(test_truthy))
