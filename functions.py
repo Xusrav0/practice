9e3aaece6c055b919e82d59a76f13a4738e3b370
Xusrav0 <xusravshohidjonov@gmail.com>
2026-09-11 18:30:38 +0900

    feat: learning function and its define and call steps

---
+++ functions.py
@@ -0,0 +1,34 @@
+''' FUNCTIONS
+(1) DEFINE AND CALL
+(2) Parametr and Argument
+(3) Keyword and default arguments
+(4) Scope
+'''
+
+print("===== DEFINE (paramentr) vs CALL (argument) =====")
+# build in function > print() type()
+# Function - reusable block of code
+# Instead of block {} in Java, JS, Python uses indentation!
+
+
+# Define - paramentr
+def greet(a):
+    pass  # hech narsa qilmasdan functiondan o'tib ketadi
+
+
+def greet(a):
+    print(f"How do you do?, {a}")
+
+
+def greeting(b):
+    print("greeting is executed")
+    return f"Hi {b}"
+
+
+# CALL - execute (argument)
+greet("Martin")
+result1 = greet("Martin")
+print("result1:", result1)  # Void function None qaytaradi
+
+result2 = greeting("Justin")
+print("result2:", result2)  # return
