a675c358bac73718a38c9a5c4a2dc8fa7385fd7f
Xusrav0 <xusravshohidjonov@gmail.com>
2026-09-11 22:11:00 +0900

    feat: completed task F

--- train.js
+++ train.js
@@ -1,3 +1,38 @@
+// TASK - F
+/*
+Yagona string argumentga ega findDoublers nomli function tuzing
+Agar stringda bittadan ortiq bir xil harflar ishtirok etgan bo'lsa
+true yokida false natija qaytarsin.
+
+MASALAN: findDoublers("hello"); natija true qaytadi. Sababi ikki marotaba takrorlangan 'll' harfi mavjud!
+
+*/
+function findDoublers(a) {
+  let b = '';
+for (let char of a) {
+  if (b.includes(char)) {
+    return true
+  } 
+  
+  b += char
+}
+return false
+}
+
+
+
+
+console.log(findDoublers('hello'))
+// console.log(findDoublers('Microsoft'))
+// console.log(findDoublers('austin'))
+
+
+
+
+
+
+
+
 // TASK - E
 
 /*
@@ -5,10 +40,11 @@ Shunday function tuzing, u bitta string argumentni qabul
 qilib osha stringni teskari qilib return qilsin.
 MASALAN: getReverse("hello") return qilsin "olleh"
 */
-
+/*
 function getReverse(a) {
   return a.split("").reverse().join("");
 }
 
 console.log(getReverse("hello"));
 console.log(getReverse("transformers"));
+*/
