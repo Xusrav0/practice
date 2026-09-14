b4945ac5a3575d31de02c0ce0bb9992a2deb5cb3
Xusrav0 <xusravshohidjonov@gmail.com>
2026-09-14 23:09:42 +0900

    feat: Completed task G

--- train.js
+++ train.js
@@ -1,3 +1,30 @@
+// TASK G:
+
+// Yagona parametrga ega function tuzing.
+// Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
+// Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+
+// MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
+// Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
+// Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
+
+function getHighestIndex(array) {
+  let b = 0;
+  let index = 0
+ for (let i = 0; i < array.length; i++) {
+  if(array[i] > b) {
+    b = array[i]
+    index = i
+  }
+ }
+ return index
+}
+
+console.log(getHighestIndex([5, 21, 12, 21 ,8]))
+
+
+
+
 // TASK - F
 /*
 Yagona string argumentga ega findDoublers nomli function tuzing
@@ -7,6 +34,7 @@ true yokida false natija qaytarsin.
 MASALAN: findDoublers("hello"); natija true qaytadi. Sababi ikki marotaba takrorlangan 'll' harfi mavjud!
 
 */
+/*
 function findDoublers(a) {
   let b = '';
 for (let char of a) {
@@ -27,7 +55,7 @@ console.log(findDoublers('hello'))
 // console.log(findDoublers('austin'))
 
 
-
+*/
 
 
 
